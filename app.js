04e9650e4203b69be61b21932b50a42bb6054132
abdulahad-Genai <154248193+abdulahad-Genai@users.noreply.github.com>
2024-01-23 00:42:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+console.log("Hello world");
+
+
+
+var inp = prompt("Enter the number");
+
+document.write(`${inp} * 1 = `, inp * 1 + "<br>");
+document.write(`${inp} * 2 = `, inp * 2 + "<br>");
+document.write(`${inp} * 3 = `, inp * 3 + "<br>");
+document.write(`${inp} * 4 = `, inp * 4 + "<br>");
+document.write(`${inp} * 5 = `, inp * 5 + "<br>");
+document.write(`${inp} * 6 = `, inp * 6 + "<br>");
+document.write(`${inp} * 7 = `, inp * 7 + "<br>");
+document.write(`${inp} * 8 = `, inp * 8 + "<br>");
+document.write(`${inp} * 9 = `, inp * 9 + "<br>");
+document.write(`${inp} * 10 = `, inp * 10 + "<br>");
+document.write(`${inp} * 11 = `, inp * 11 + "<br>");
+document.write(`${inp} * 12 = `, inp * 12 + "<br>");
+document.write(`${inp} * 13 = `, inp * 13 + "<br>");
+document.write(`${inp} * 14 = `, inp * 14 + "<br>");
+document.write(`${inp} * 15 = `, inp * 15 + "<br>");
+document.write(`${inp} * 16 = `, inp * 16 + "<br>");
+document.write(`${inp} * 17 = `, inp * 17 + "<br>");
+document.write(`${inp} * 18 = `, inp * 18 + "<br>");
+document.write(`${inp} * 19 = `, inp * 19 + "<br>");
+document.write(`${inp} * 20 = `, inp * 20 + "<br>");
